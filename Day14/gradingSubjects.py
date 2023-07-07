c69110d34472a00d2ca47a20f95dad78d701d1bc
Zawaddie <zeddieburu87@gmail.com>
2023-07-07 15:23:27 -0400

    day 14 commit

--- Day14/gradingSubjects.py
+++ Day14/gradingSubjects.py
@@ -1,4 +1,4 @@
-def calculate_grade(score):
+def calculateGrade(score):
     if score >= 90:
         return 'A'
     elif score >= 80:
@@ -10,10 +10,15 @@ def calculate_grade(score):
     else:
         return 'F'
 
+def findSum(arr):
+    curr =0
+    for val in arr:
+        curr += val
+    return curr
 
-def calculate_overall_grade(grades):
-    overall_mean = sum(grades) / len(grades)
-    overall_grade = calculate_grade(overall_mean)
+def calculateOverallGrade(grades):
+    overall_mean = findSum(grades) / len(grades)
+    overall_grade = calculateGrade(overall_mean)
     return overall_mean, overall_grade
 
 
@@ -24,21 +29,34 @@ def validate_score(score):
     return True
 
 
-subjects = ['Subject 1', 'Subject 2', 'Subject 3', 'Subject 4', 'Subject 5']
+subjects = ['Maths', 'English', 'Kiswahili', 'Science', 'Geography']
 grades = []
 
 for subject in subjects:
     while True:
-        score = float(input(f"Enter the score for {subject}: "))
-        if validate_score(score):
-            break
-    grade = calculate_grade(score)
-    grades.append(score)
-    print(f"{subject}: {grade}")
+        try:
+            score = float(input(f"Enter the score for {subject}: "))
+            if validate_score(score):
+                grades.append(score)
+                break
+            # grade = calculateGrade(score)
+            # print(f"{subject}: {grade}")
+        except:
+            print("Only Integer are required")
 
-overall_mean, overall_grade = calculate_overall_grade(grades)
-total_marks = sum(grades)
 
-print(f"\nOverall Mean: {overall_mean}")
-print(f"Overall Grade: {overall_grade}")
+overall_mean, overall_grade = calculateOverallGrade(grades)
+total_marks = findSum(grades)
+
+
+#print(f"\nScores in summary: {subjects}{grades}")
+sorted_scores = sorted(grades)
+#print("Sorted Scores:", sorted_scores)
+
+print("\n\tHey Zawadi, Here is summary of your score:\n")
+print("SUBJECT \tSCORE  \tGRADE")
+for score in sorted_scores:
+    subject = subjects[grades.index(score)]
+    print(f"{subject} \t{score} \t{calculateGrade(score)}")
+print(f"Overall   \t{overall_mean}\t{overall_grade}")
 print(f"Total Marks: {total_marks}")
